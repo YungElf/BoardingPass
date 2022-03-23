c162cc0eb68954585b6a9108beef7f65a3dd5ce4
Alex R <alexgro.public@gmail.com>
2022-03-23 12:33:25 -0700

    Phone Number + Person
    Configured Phone Number Handling and the Person class

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,81 @@
 package person;
 
+import boardingpass.BoardingPass;
+
+import java.util.ArrayList;
+
 public class Person {
+    private String name;
+    private String email;
+    private int age;
+    private Sex sex;
+    private PhoneNumber phoneNumber;
+    private boolean discount;
+    private ArrayList<BoardingPass> boardingPass;
+
+
+    public Person(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public Sex getSex(){
+        return sex;
+    }
+
+    public PhoneNumber getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public boolean getDiscount(){
+        return discount;
+    }
+
+
+    //Setters
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setDiscount(int age,Sex sex){
+        if (this.getAge() <= 12){
+            this.discount = true;
+        }
+        if(this.getAge() >= 65 ){
+            this.discount= true;
+        }
+        if(this.getSex() == Sex.FEMALE){
+            this.discount = true;
+        }
+        else
+            this.discount = false;
+    }
 }
+
+
